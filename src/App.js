760633a904c7f528e29f26957d8b39569a52bf6f
Flavio Macedo <fhenrique609@gmail.com>
2024-06-25 09:36:50 -0300

    Layout Responsivo

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import logo from './cacex-logo.jpeg'
 function App() {
   return (
     <div className="App">
-      <div className="container-app">
-        <div className="left flex flex-col h-screen">
-          <img src={logo} alt="CACEX Logo" className="logo"/>
+      <div className="container-app flex flex-col md:flex-row h-screen">
+        <div className="left flex-col">
+          <img src={logo} alt="CACEX Logo" className="logo md:mb-0"/>
           <button className="mt-4 shadow btn-grad focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 rounded">
             Cadastrar
           </button>
         </div>
-        <div className="right">
+        <div className="right flex-1 flex items-center justify-center bg-gray-100 p-4 md:p-12">
           <div className="login-box">
             <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
             <form>
@@ -27,7 +27,7 @@ function App() {
                 placeholder="Senha"
                 className="border border-gray-300 p-2 rounded w-full mb-6 focus:outline-none focus:border-blue-500"
               />
-              <button className="shadow btn-grad focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+              <button className="shadow btn-grad focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded w-full" type="button">
                 Login
               </button>
             </form>
